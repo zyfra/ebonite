88aefd8f16114ab6c5159630dbf7dbcc0d222d39
Ivan Andrianov <Ivan.Andrianov@zyfra.com>
2019-11-20 14:36:00 +0300

    EBNT-126 expose PyTorch extensions in package's __all__

---
+++ src/ebonite/ext/torch/__init__.py
@@ -0,0 +1,5 @@
+from .dataset import TorchTensorDatasetType, TorchTensorHook, TorchTensorListDatasetType
+from .model import TorchModelHook, TorchModelWrapper
+
+__all__ = ['TorchTensorDatasetType', 'TorchTensorHook', 'TorchTensorListDatasetType',
+           'TorchModelHook', 'TorchModelWrapper']
