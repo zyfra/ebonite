6582456c8fff87decf6f14c9965a26af8abb441a
Ivan Andrianov <Ivan.Andrianov@zyfra.com>
2019-12-02 15:24:17 +0300

    EBNT-150 tests for Tensorflow model wrapper

---
+++ tests/ext/tensorflow/test_model.py
@@ -0,0 +1,48 @@
+import pytest
+import tensorflow as tf
+
+from ebonite.core.analyzer.model import ModelAnalyzer
+
+
+def test_tf__single_tensor(graph, tensor, tmpdir):
+    with graph.as_default():
+        out = tf.layers.dense(tensor, 1, activation=tf.tanh)
+
+    _check_model_wrapper(graph, out, {tensor.name: [[1]]}, tmpdir)
+
+
+def test_tf__multiple_tensors(graph, tensor, second_tensor, tmpdir):
+    with graph.as_default():
+        cat = tf.concat([tensor, second_tensor], -1)
+        out1 = tf.layers.dense(cat, 1, activation=tf.tanh)
+        out2 = tf.layers.dense(cat, 1, activation=tf.sigmoid)
+
+    _check_model_wrapper(graph, [out1, out2], {tensor.name: [[1]], second_tensor.name: [[1, 2]]}, tmpdir)
+
+
+def _check_model_wrapper(graph, model, feed_dict, tmpdir):
+    # this import is required to ensure that Tensorflow model wrapper is registered
+    import ebonite.ext.tensorflow  # noqa
+
+    with graph.as_default():
+        tmw = ModelAnalyzer.analyze(model)
+        assert tmw.model is model
+
+    with tf.Session(graph=graph) as session:
+        session.run(tf.global_variables_initializer())
+        # training here is just random initialization
+
+        pred = tmw.predict(feed_dict)
+
+        with tmw.dump() as artifact:
+            artifact.materialize(tmpdir)
+
+        tmw.unbind()
+        with pytest.raises(ValueError):
+            tmw.predict(feed_dict)
+
+    tmw.load(tmpdir)
+    assert tmw.model is not model
+
+    pred2 = tmw.predict(feed_dict)
+    assert pred2 == pred
