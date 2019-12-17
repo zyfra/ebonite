cd6387da07347e2ed7b9b032fbcb320cb2a332ca
Ivan Andrianov <Ivan.Andrianov@zyfra.com>
2019-12-17 14:50:25 +0300

    EBNT-157 support for predict_proba in sklearn

--- src/ebonite/ext/sklearn/model.py
+++ src/ebonite/ext/sklearn/model.py
@@ -1,4 +1,5 @@
 from pyjackson.decorators import make_string
+from sklearn.base import ClassifierMixin
 
 from ebonite.core.analyzer.base import LibHookMixin
 from ebonite.core.analyzer.model import ModelHook
@@ -13,6 +14,15 @@ class SklearnModelWrapper(PickleModelWrapper):
     @ModelWrapper.with_model
     def predict(self, data):
         return self.model.predict(data)
+
+    def __getattr__(self, item):
+        if item == 'predict_proba' and isinstance(self.model, ClassifierMixin):
+            return self._predict_proba
+        raise AttributeError(f"'{type(self)}' object has not attribute '{item}'")
+
+    @ModelWrapper.with_model
+    def _predict_proba(self, data):
+        return self.model.predict_proba(data)
 
 
 @make_string(include_name=True)
--- tests/ext/sklearn/test_model.py
+++ tests/ext/sklearn/test_model.py
@@ -1,44 +1,78 @@
+import numpy as np
 import pytest
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 
 from ebonite.core.analyzer.model import ModelAnalyzer
 from ebonite.ext.sklearn import SklearnModelWrapper
 
 
 @pytest.fixture
-def data():
-    return [[1, 2, 3]]
+def inp_data():
+    return [[1, 2, 3], [3, 2, 1]]
 
 
 @pytest.fixture
-def model(data):
+def out_data():
+    return [1, 2]
+
+
+@pytest.fixture
+def classifier(inp_data, out_data):
+    lr = LogisticRegression()
+    lr.fit(inp_data, out_data)
+    return lr
+
+
+@pytest.fixture
+def regressor(inp_data, out_data):
     lr = LinearRegression()
-    lr.fit(data, data)
+    lr.fit(inp_data, out_data)
     return lr
 
 
 @pytest.fixture
 def wrapper(model):
-    return ModelAnalyzer.analyze(model)
+    return
 
 
-def test_hook(wrapper):
+@pytest.mark.parametrize('model', ['classifier', 'regressor'])
+def test_hook(model, request):
+    model = request.getfixturevalue(model)
+    wrapper = ModelAnalyzer.analyze(model)
+
     assert isinstance(wrapper, SklearnModelWrapper)
 
 
-def test_wrapper__predict(wrapper, data):
+@pytest.mark.parametrize('model', ['classifier', 'regressor'])
+def test_wrapper__predict(model, inp_data, request):
+    model = request.getfixturevalue(model)
+    wrapper = ModelAnalyzer.analyze(model)
 
-    prediction = wrapper.predict(data)
-
-    assert len(prediction) == len(data)
+    np.testing.assert_array_almost_equal(model.predict(inp_data), wrapper.predict(inp_data))
 
 
-def test_wrapper__dump_load(tmpdir, wrapper, data):
+def test_wrapper__clf_predict_proba(classifier, inp_data):
+    wrapper = ModelAnalyzer.analyze(classifier)
+
+    np.testing.assert_array_almost_equal(classifier.predict_proba(inp_data), wrapper.predict_proba(inp_data))
+
+
+def test_wrapper__reg_predict_proba(regressor, inp_data):
+    wrapper = ModelAnalyzer.analyze(regressor)
+
+    assert not hasattr(wrapper, 'predict_proba')
+
+
+@pytest.mark.parametrize('model', ['classifier', 'regressor'])
+def test_wrapper__dump_load(tmpdir, model, inp_data, request):
+    model = request.getfixturevalue(model)
+    wrapper = ModelAnalyzer.analyze(model)
+
     with wrapper.dump() as d:
         d.materialize(tmpdir)
     wrapper.unbind()
     with pytest.raises(ValueError):
-        wrapper.predict(data)
+        wrapper.predict(inp_data)
 
     wrapper.load(tmpdir)
-    test_wrapper__predict(wrapper, data)
+    np.testing.assert_array_almost_equal(model.predict(inp_data), wrapper.predict(inp_data))
