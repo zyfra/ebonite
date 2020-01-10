7c2cf43b422135f741f1a5904c4cda102b4f457c
mike0sv <mike0sv@gmail.com>
2020-01-10 17:49:00 +0300

    Bump version: 0.3.2 → 0.3.3

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ def get_requirements(file_name):
 
 setup_args = dict(
     name='ebonite',
-    version='0.3.2',
+    version='0.3.3',
     license='Apache-2.0',
     description='Machine Learning Lifecycle Framework',
     long_description='%s\n%s' % (
