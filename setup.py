4e5432f3c53ab8cf6b4e17a31c5f85d0e4dbe0ff
mike0sv <mike0sv@gmail.com>
2020-01-14 16:39:17 +0300

    EBNT-176 fixed torch cpu installation in tox.ini

--- setup.py
+++ setup.py
@@ -72,6 +72,9 @@ setup_args = dict(
         #   ':python_version=="2.6"': ['argparse'],
         'testing': get_requirements('test.requirements.txt')
     },
+    dependency_links=[
+        'https://download.pytorch.org/whl/torch_stable.html'
+    ],
     setup_requires=[
         'pytest-runner',
     ],
