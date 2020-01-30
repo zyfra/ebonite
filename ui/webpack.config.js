f7afa01110845c662c4499a89e203a81ec927906
Ivan Andrianov <Ivan.Andrianov@zyfra.com>
2020-01-30 17:12:02 +0300

    EBNT-170 initial state of UI

---
+++ ui/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    externals: /^(\$|React|ReactDOM)$/i
+};
