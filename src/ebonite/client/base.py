0dd7e6bea1e0adf3705d88bc9c38eaa770a973c5
Ivan Andrianov <Ivan.Andrianov@zyfra.com>
2020-02-28 17:50:14 +0300

    EBNT-246 EBNT-169 use build_model_flask_docker by default

--- src/ebonite/client/base.py
+++ src/ebonite/client/base.py
@@ -115,8 +115,13 @@ class Ebonite:
         :param model: model to wrap into service
         :return: :class:`~ebonite.core.objects.Image` instance representing built image
         """
-        from ebonite.build import build_model_docker
-        image = build_model_docker(name, model, **kwargs)
+        if 'server' not in kwargs:  # by default we use uwsgi flask server
+            from ebonite.ext.flask.helpers import build_model_flask_docker
+            kwargs = {k: v for k, v in kwargs.items() if k in {'force_overwrite', 'debug'}}
+            image = build_model_flask_docker(name, model, **kwargs)
+        else:
+            from ebonite.build import build_model_docker
+            image = build_model_docker(name, model, **kwargs)
         return self.meta_repo.create_image(image)
 
     @abstractmethod
