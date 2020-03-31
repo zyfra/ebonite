16b4ae4e41fd2f502c81771b222b6cdf7d5de56c
Ivan Andrianov <Ivan.Andrianov@zyfra.com>
2020-03-31 15:17:20 +0300

    EBNT-264 test sql repo with both in-memory sqlite and postgresql

--- tests/ext/sqlalchemy/test_sqlite/conftest.py
+++ tests/ext/sqlalchemy/test_sqlite/conftest.py
@@ -5,8 +5,8 @@ from tests.repository.metadata.conftest import create_metadata_hooks
 
 
 @pytest.fixture
-def sqlalchemy_meta():
+def sqlite_meta():
     return SQLAlchemyMetaRepository('sqlite://')
 
 
-pytest_runtest_protocol, pytest_collect_file = create_metadata_hooks(sqlalchemy_meta, 'sqlalchemy')
+pytest_runtest_protocol, pytest_collect_file = create_metadata_hooks(sqlite_meta, 'sqlite')
