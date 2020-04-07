6980cf4019651c350d4a8239f7fb4f2baecb2d77
ElmiRakh <elmirakh.git@gmail.com>
2020-04-08 01:30:47 +0700

    EBNT-271 Refactored and added doocs

--- src/ebonite/api/api_base.py
+++ src/ebonite/api/api_base.py
@@ -1,3 +1,5 @@
+from typing import List, Callable
+
 from flask import Flask, Response
 
 from ebonite.build.docker import is_docker_running
@@ -6,9 +8,18 @@ from ebonite.repository.artifact.base import NoSuchArtifactError
 
 
 class EboniteApi:
-    app = None
+    """
+    API that provides ability to interact with Ebonite object. Based on Flask framework
+    :param name: name of the flask application
+    :param config_path: path to the config which will be used to initialize Ebonite object
+    :param host: host on which API will be run
+    :param port: port on which API will be run
+    :param debug: Control of mode in which Flask application will be run
 
-    def __init__(self, name, config_path, host='127.0.0.1', port='5000', debug=True):
+    """
+    app: Flask = None
+
+    def __init__(self, name: str, config_path: str, host: str = '127.0.0.1', port: str = '5000', debug: bool = True):
         self.app = Flask(name)
         self.host = host
         self.port = port
@@ -36,17 +47,26 @@ class EboniteApi:
     def init_ebonite(self):
         self.ebonite = Ebonite.from_config_file(self.config_path)
 
-    def add_endpoint(self, endpoint=None, endpoint_name=None, handler=None, methods=['GET']):
+    def add_endpoint(self, endpoint: str = None, endpoint_name: str = None, handler: Callable = None,
+                     methods: List[str] = ['GET']):
         self.app.add_url_rule(endpoint, endpoint_name, handler, methods=methods)
 
     @staticmethod
-    def docker_healthcheck():
+    def docker_healthcheck() -> Response:
+        """
+        Function which checks if Docker daemon is ready to use
+        :return: Response object which signifies if daemon is healthy
+        """
         if is_docker_running():
             return Response(status=200, response='Docker daemon is healthy')
         else:
             return Response(status=404, response='Failed to establish connection to docker daemon')
 
-    def metadata_healthcheck(self):
+    def metadata_healthcheck(self) -> Response:
+        """
+        Checks if metadata repository is healthy by trying to get any project from it
+        :return: Response object which signifies if metadata repository is healthy
+        """
         try:
             self.ebonite.meta_repo.get_project_by_id(1)
             return Response(status=200, response='Metadata repository is healthy')
@@ -55,7 +75,12 @@ class EboniteApi:
             return Response(status=404, response=f'Error {e} while trying to '
                                                  f'establish connection to metadata repository')
 
-    def artifact_healthcheck(self):
+    def artifact_healthcheck(self) -> Response:
+        """
+        Checks if artifact repository is healthy by trying to get any artifact from it.
+        Might return NoSuchArtifactError which signifies that repository is ready to use
+        :return:  Response object which signifies if artifact repository is healthy
+        """
         try:
             self.ebonite.artifact_repo._get_artifact('1')
             return Response(status=200, response='Artifact repository is healthy')
