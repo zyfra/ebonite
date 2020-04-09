2249a4a161fb8d7642ad9860bf5398e8d51a7590
ElmiRakh <elmirakh.git@gmail.com>
2020-04-09 23:59:36 +0700

    EBNT-272 Api examples refactoring

--- examples/api_test/api_server.py
+++ examples/api_test/api_server.py
@@ -1,8 +1,9 @@
-from ebonite.api.api_base import EboniteApi
 import os
+
+from ebonite.api.api_base import EboniteApi
 
 os.environ['S3_ACCESS_KEY'] = 'eboniteAccessKey'
 os.environ['S3_SECRET_KEY'] = 'eboniteSecretKey'
 
-api = EboniteApi(name='test',config_path='client_config.json')
+api = EboniteApi(name='test', config_path='client_config.json')
 api.run()
--- examples/api_test/client.py
+++ examples/api_test/client.py
@@ -1,7 +1,8 @@
 import json
+
 import requests
 
-#Healthchecks
+# Healthchecks
 docker_healthcheck = requests.get('http://127.0.0.1:5000/healthcheck/docker')
 print(docker_healthcheck.status_code)
 print(docker_healthcheck.content)
@@ -12,27 +13,26 @@ artifact_healthcheck = requests.get('http://127.0.0.1:5000/healthcheck/artifact'
 print(artifact_healthcheck.status_code)
 print(artifact_healthcheck.content)
 
-#create project with error cases
-create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name':'proj1'}))
+# create project with error cases
+create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name': 'proj1'}))
 print(create_project.status_code)
 print(create_project.content)
-create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name':'proj2'}))
+create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name': 'proj2'}))
 print(create_project.status_code)
 print(create_project.content)
-create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name':'proj3'}))
-print(create_project.status_code)
-print(create_project.content)
-
-create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'asd':'aaaa'}))
+create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name': 'proj3'}))
 print(create_project.status_code)
 print(create_project.content)
 
-
-create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name':'aaaa'}))
+create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'asd': 'aaaa'}))
 print(create_project.status_code)
 print(create_project.content)
 
-create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps([{'name':'b'}, {'name':'a'}]))
+create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps({'name': 'aaaa'}))
+print(create_project.status_code)
+print(create_project.content)
+
+create_project = requests.post('http://127.0.0.1:5000/projects', data=json.dumps([{'name': 'b'}, {'name': 'a'}]))
 print(create_project.status_code)
 print(create_project.content)
 
@@ -47,7 +47,7 @@ print(get_project_by_id.status_code)
 print(get_project_by_id.content)
 
 print('Update project')
-update_project = requests.patch('http://127.0.0.1:5000/projects/1',data=json.dumps({'name':'ddd'}))
+update_project = requests.patch('http://127.0.0.1:5000/projects/1', data=json.dumps({'name': 'ddd'}))
 print(update_project.status_code)
 print(update_project.content)
 
