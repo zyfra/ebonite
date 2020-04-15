313170e99acb79bb271b94323452ba4736432193
Mikhail Sveshnikov <mike0sv@gmail.com>
2020-04-15 16:35:54 +0300

    EBNT-295 update notebook and some small fixes (#81)
    * EBNT-295 update notebook and some small fixes
    
    * EBNT-295 nb improvement and docker image name fix

--- examples/sklearn_model/model_create_image.py
+++ examples/sklearn_model/model_create_image.py
@@ -1,7 +1,6 @@
 """This example shows how you can use ebonite to wrap your sklearn model into docker container"""
 
 from ebonite import Ebonite
-from ebonite.build.builder.base import use_local_installation
 
 
 def main():
@@ -11,12 +10,9 @@ def main():
     task = ebnt.get_or_create_task('my_project', 'regression_is_my_profession')
     model = task.models('mymodel')
 
-    #  this changes docker image builder behaviour to get ebonite from local installation instead of pip
-    #  1. for developing reasons 2. we dont have ebonite on pip yet
-    with use_local_installation():
-        #  build docker image from model and run it
-        ebnt.build_and_run_instance("sklearn_model_service", model, detach=False, force_overwrite=True)
-        # now you can use client.py to call this service or go to http://localhost:9000/apidocs to view swagger ui
+    #  build docker image from model and run it
+    ebnt.build_and_run_instance("sklearn_model_service", model, detach=False, force_overwrite=True)
+    # now you can use client.py to call this service or go to http://localhost:9000/apidocs to view swagger ui
 
 
 if __name__ == '__main__':
