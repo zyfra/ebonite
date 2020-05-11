6d3c5c58cef898d215af674fd335c38d65ed4ee5
ElmiRakh <elmirakh.git@gmail.com>
2020-05-11 18:02:02 +0700

    EBNT fixed use_local_installation changing cwd

--- src/ebonite/build/builder/base.py
+++ src/ebonite/build/builder/base.py
@@ -93,10 +93,12 @@ class PythonBuilder(BuilderBase):
             requirements = self.provider.get_requirements()
             logger.debug('Auto-determined requirements for model: %s.', requirements.to_pip())
             if not ebonite_from_pip():
+                cwd = os.getcwd()
                 from setup import setup_args  # FIXME only for development
                 requirements += list(setup_args['install_requires'])
                 logger.debug('Adding Ebonite requirements as local installation is employed...')
                 logger.debug('Overall requirements for model: %s.', requirements.to_pip())
+                os.chdir(cwd)
             req.write('\n'.join(requirements.to_pip()))
 
     def _write_run_script(self, target_dir):
