42014f605e009795a223130dafdc0e96b85a6d5f
Elmir Akhmedov <elmirakh.git@gmail.com>
2020-05-15 21:03:50 +0700

    Ebnt 321 images delete (#92)
    * EBNT fixed use_local_installation changing cwd
    
    * EBNT-321 Added docker image removal in metadata repository
    
    * EBNT Added try-finally block
    
    * EBNT-321 Moved image deletion to builder
    
    * EBNT-321 tox fix
    
    * EBNT fixes for new flake8 version
    
    * EBNT-321 Moved image removal to Environment params
    
    * EBNT-321 Added tests
    
    * EBNT-321 tox fix
    
    * EBNT Diverted one of the changes for tox
    
    * EBNT Fixes for tox
    
    * EBNT Fixes for tox
    
    * EBNT-321 Changed image removal / Added simpler test for it
    
    * EBNT-321 removed obsolete code
    
    * EBNT-321 Decoupled tests changed / added host_only param to delete images only from image host
    
    * EBNT-321 Post-review fixes
    
    * EBNT-321 Resolved conficts

--- tests/client/conftest.py
+++ tests/client/conftest.py
@@ -1,8 +1,9 @@
 import time
 
 import pytest
+from pyjackson.core import Comparable
 
-from ebonite.core.objects.core import Model
+from ebonite.core.objects.core import Image, Model
 from tests.build.builder.test_docker import has_docker
 from tests.build.conftest import rm_container, rm_image
 from tests.client.test_func import func
@@ -11,6 +12,24 @@ from tests.conftest import interface_hook_creator
 CONTAINER_NAME = "ebonite-test-service"
 
 CLEAR = True  # flag to disable removal of containers for easy inspection and debugging
+
+
+class MockEnvironmentParams(Comparable):
+    default_runner = None
+
+    def get_runner(self):
+        # TODO: Runner
+        return self.default_runner
+
+    def get_builder(self, name: str, model: Model, server, debug=False, **kwargs):
+        # TODO: Builder
+        return
+
+    def remove_image(self, image: Image):
+        if image.params.name:
+            return True
+        else:
+            return False
 
 
 @pytest.fixture
@@ -30,6 +49,16 @@ def container_name():
 def model():
     model = Model.create(func, "kek", "Test Model")
     return model
+
+
+@pytest.fixture
+def image_to_delete(ebnt, model):
+    task = ebnt.get_or_create_task('Project', 'Task')
+    model = ebnt.push_model(model, task)
+    image = Image('image', id=None, model_id=model.id, params=Image.Params())
+    image.params.name = 'image'
+    image = ebnt.meta_repo.create_image(image)
+    yield image
 
 
 create_client_hooks = interface_hook_creator('tests/client/', 'client_common.py', 'ebnt')
