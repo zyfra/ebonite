05f898bc387ab395310d5c4dd7a98150bc6a90de
ElmiRakh <elmirakh.git@gmail.com>
2020-06-09 20:35:26 +0700

    EBNT-371 sklearn_model example fix

--- examples/sklearn_model/model_create_image.py
+++ examples/sklearn_model/model_create_image.py
@@ -1,23 +1,22 @@
 """This example shows how you can use ebonite to wrap your sklearn model into docker container"""
 
 from ebonite import Ebonite
-from ebonite.build.builder.base import use_local_installation
 
 def main():
     #  create local ebonite client. This client stores metadata and artifacts on local fs.
-    with use_local_installation():
-        ebnt = Ebonite.local()
 
-        task = ebnt.get_or_create_task('my_project', 'regression_is_my_profession')
-        model = task.models('mymodel')
+    ebnt = Ebonite.local()
 
-        #  build docker image from model and run it
-        ebnt.build_and_run_instance("sklearn_model_service", model, task,
-                                    runner_kwargs={'detach': False},
-                                    builder_kwargs={'force_overwrite': True},
-                                    instance_kwargs={'port_mapping': {9000:9000}}
-                                    )
-        # now you can use client.py to call this service or go to http://localhost:9000/apidocs to view swagger ui
+    task = ebnt.get_or_create_task('my_project', 'regression_is_my_profession')
+    model = task.models('mymodel')
+
+    #  build docker image from model and run it
+    ebnt.build_and_run_instance("sklearn_model_service", model, task,
+                                runner_kwargs={'detach': False},
+                                builder_kwargs={'force_overwrite': True},
+                                instance_kwargs={'port_mapping': {9000:9000}}
+                                )
+    # now you can use client.py to call this service or go to http://localhost:9000/apidocs to view swagger ui
 
 
 if __name__ == '__main__':
