2b3551b9b8966bd951e3ea3b8fee1772bb194691
mike0sv <mike0sv@gmail.com>
2020-06-12 01:43:30 +0300

    Bump version: 0.5.2 → 0.6.0

--- src/ebonite/__init__.py
+++ src/ebonite/__init__.py
@@ -8,7 +8,7 @@ if config.Core.AUTO_IMPORT_EXTENSIONS:
     ExtensionLoader.load_all()
 
 __all__ = ['load_extensions', 'Ebonite', 'EBONITE_DEBUG', 'start_runtime', 'create_model']
-__version__ = '0.5.2'
+__version__ = '0.6.0'
 
 if __name__ == '__main__':
     pass
