3423d65eb3967c07e1bb442a8a96be201bd83415
mike0sv <mike0sv@gmail.com>
2020-06-18 00:37:35 +0300

    Bump version: 0.6.1 → 0.6.2

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ project = 'Ebonite'
 year = '2019 - 2020'
 author = 'Zyfra'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.6.1'
+version = release = '0.6.2'
 
 pygments_style = 'trac'
 templates_path = ['.']
